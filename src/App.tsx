6eb3c37fa4f8d4c2c4788814181fe2e9142ac204
Neh333 <124055223+Neh333@users.noreply.github.com>
2024-08-07 02:34:17 -0400

    make image a legit bg

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <div>
-        {/*backgroundImage: `url(${process.env.PUBLIC_URL + '~/src/TempStreamBg.jpg'})`,*/}
-
+      <>
+        <div className='bg'></div>
         <h1>The Evolution Archive</h1>
-      </div>
+      </>
       
-    </>
+  
   )
 }
 
