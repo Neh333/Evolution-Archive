3253f4234deda70af037df3b5b447b87ed94c23f
Neh333 <nehemiahhughes05@gmail.com>
2024-08-08 23:01:40 -0400

    bg BUT overflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import  './App.css'
 
-import background from "/img/placeholder.png";
-
-
 function App() {
 
   return (
       <>
-       
-        <img alt="image" src="background"/>
-
         <div className='homepage-bg'>
-          <h1>The Evolution Archive</h1>
+          <h1 className='text-center'>The Evolution Archive</h1>
         </div>
-        
       </>
       
   )
